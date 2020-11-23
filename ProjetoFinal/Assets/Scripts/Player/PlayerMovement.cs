772c9ffd2607dccad6f2c2e3811c55ab249f83e8
De4dSh0t <ruipsilva2001@hotmail.com>
2020-11-23 17:16:53 +0000

    Added PlayerMovement that invokes the movement commands

---
+++ ProjetoFinal/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    
+    //Movement Commands
+    private MoveUp up;
+    private MoveLeft left;
+    private MoveDown down;
+    private MoveRight right;
+    
+    void Start()
+    {
+        up = new MoveUp(transform, speed);
+        left = new MoveLeft(transform, speed);
+        down = new MoveDown(transform, speed);
+        right = new MoveRight(transform, speed);
+    }
+    
+    void Update()
+    {
+        HandleMoveInput();
+    }
+
+    private void HandleMoveInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            up.Execute();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            left.Execute();
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            down.Execute();
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            right.Execute();
+        }
+    }
+}
