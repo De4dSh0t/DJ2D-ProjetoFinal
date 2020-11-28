1ef97ad546ce19577701cf249171c531975a33cc
De4dSh0t <ruipsilva2001@hotmail.com>
2020-11-28 18:40:34 +0000

    Updated collision detection to prevent clipping

--- ProjetoFinal/Assets/Scripts/Collision/CollisionDetector.cs
+++ ProjetoFinal/Assets/Scripts/Collision/CollisionDetector.cs
@@ -3,6 +3,7 @@
 public class CollisionDetector : MonoBehaviour
 {
     [Header("Collision Settings")] 
+    [SerializeField] private Vector2 size;
     [SerializeField] private Vector2 offset;
     [SerializeField] private float distance;
     [SerializeField] private LayerMask collisionLayer;
@@ -17,31 +18,37 @@ public class CollisionDetector : MonoBehaviour
         CheckCollision();
     }
 
-    private void CheckCollision() // TODO: Fix clipping
+    private void CheckCollision()
     {
         Vector2 currentPos = transform.position;
         
         //Up
-        canMoveUp = !Physics2D.Raycast(currentPos - new Vector2(offset.x, 0), Vector2.up, distance, collisionLayer) &&
-                    !Physics2D.Raycast(currentPos + new Vector2(offset.x, 0), Vector2.up, distance, collisionLayer);
+        canMoveUp = !Physics2D.Raycast(currentPos + new Vector2(offset.x, size.y), Vector2.left, distance, collisionLayer);
         
         //Left
-        canMoveLeft = !Physics2D.Raycast(currentPos - new Vector2(0, offset.y), Vector2.left, distance, collisionLayer) &&
-                      !Physics2D.Raycast(currentPos + new Vector2(0, offset.y), Vector2.left, distance, collisionLayer);
+        canMoveLeft = !Physics2D.Raycast(currentPos - new Vector2(size.x, size.y - (size.y - offset.y)), Vector2.up, distance, collisionLayer);
         
         //Down
-        canMoveDown = !Physics2D.Raycast(currentPos - new Vector2(offset.x, 0), Vector2.down, distance, collisionLayer) &&
-                      !Physics2D.Raycast(currentPos + new Vector2(offset.x, 0), Vector2.down, distance, collisionLayer);
+        canMoveDown = !Physics2D.Raycast(currentPos - new Vector2(size.x - (size.x - offset.x), size.y), Vector2.right, distance, collisionLayer);
         
         //Right
-        canMoveRight = !Physics2D.Raycast(currentPos - new Vector2(0, offset.y), Vector2.right, distance, collisionLayer) &&
-                       !Physics2D.Raycast(currentPos + new Vector2(0, offset.y), Vector2.right, distance, collisionLayer);
+        canMoveRight = !Physics2D.Raycast(currentPos + new Vector2(size.x, size.y - (size.y - offset.y)), Vector2.down, distance, collisionLayer);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine((Vector2) transform.position - new Vector2(0, offset.y), (Vector2) transform.position - new Vector2(0, offset.y) + Vector2.left * distance);
-        Gizmos.DrawLine((Vector2) transform.position + new Vector2(0, offset.y), (Vector2) transform.position + new Vector2(0, offset.y) + Vector2.left * distance);
+        
+        //Up
+        Gizmos.DrawLine((Vector2) transform.position + new Vector2(offset.x, size.y), (Vector2) transform.position + new Vector2(offset.x, size.y) + Vector2.left * distance);
+        
+        //Left
+        Gizmos.DrawLine((Vector2) transform.position - new Vector2(size.x, size.y - (size.y - offset.y)), (Vector2) transform.position - new Vector2(size.x, size.y - (size.y - offset.y)) + Vector2.up * distance);
+        
+        //Down
+        Gizmos.DrawLine((Vector2) transform.position - new Vector2(size.x - (size.x - offset.x), size.y), (Vector2) transform.position - new Vector2(size.x - (size.x - offset.x), size.y) + Vector2.right * distance);
+        
+        //Right
+        Gizmos.DrawLine((Vector2) transform.position + new Vector2(size.x, size.y - (size.y - offset.y)), (Vector2) transform.position + new Vector2(size.x, size.y - (size.y - offset.y)) + Vector2.down * distance);
     }
 }
