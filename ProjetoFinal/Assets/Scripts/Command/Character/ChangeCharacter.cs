7a27810967823ad96d56bd5b16de2f2a6f536380
De4dSh0t <ruipsilva2001@hotmail.com>
2020-11-28 19:23:20 +0000

    Added CharacterChanger using the command pattern

---
+++ ProjetoFinal/Assets/Scripts/Command/Character/ChangeCharacter.cs
@@ -0,0 +1,17 @@
+﻿public class ChangeCharacter : ICommand
+{
+    private readonly PlayerMovement current;
+    private readonly PlayerMovement target;
+
+    public ChangeCharacter(PlayerMovement current, PlayerMovement target)
+    {
+        this.current = current;
+        this.target = target;
+    }
+
+    public void Execute()
+    {
+        current.enabled = false;
+        target.enabled = true;
+    }
+}
---
+++ ProjetoFinal/Assets/Scripts/Player/CharacterChanger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterChanger : MonoBehaviour
+{
+    [Header("Character Settings")]
+    [SerializeField] private PlayerMovement[] characters;
+    private int index;
+    
+    //Components
+    private ChangeCharacter change;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ChangeCharacter();
+        }
+    }
+
+    private void ChangeCharacter()
+    {
+        change = new ChangeCharacter(characters[index], characters[NextIndex()]);
+        change.Execute();
+    }
+
+    private int NextIndex()
+    {
+        index++;
+        
+        if (index > characters.Length - 1)
+        {
+            index = 0;
+        }
+        
+        return index;
+    }
+}
