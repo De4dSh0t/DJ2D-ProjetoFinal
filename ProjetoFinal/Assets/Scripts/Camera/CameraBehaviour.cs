ca5f5776a414de9ef3d1be25189ab997531ee55f
De4dSh0t <ruipsilva2001@hotmail.com>
2020-11-30 18:26:41 +0000

    Added CameraBehaviour that follows the current target

---
+++ ProjetoFinal/Assets/Scripts/Camera/CameraBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private Transform target;
+    [SerializeField] private float smooth;
+
+    [Header("Camera Settings")]
+    [SerializeField] private Vector3 offset;
+    private Camera currentCamera;
+
+    void Start()
+    {
+        currentCamera = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        //Smoothly follow the current target
+        currentCamera.transform.position = Vector3.Lerp(currentCamera.transform.position, target.position + offset, smooth);
+    }
+    
+    public void UpdateTarget(Transform nextTarget)
+    { 
+        target = nextTarget;
+    }
+}
