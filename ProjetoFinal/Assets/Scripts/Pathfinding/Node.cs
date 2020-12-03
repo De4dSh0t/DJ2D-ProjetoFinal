c6a66f0ee13e368ebfba86a92bb8565f3a8167cf
De4dSh0t <ruipsilva2001@hotmail.com>
2020-12-03 14:53:20 +0000

    Started working on A* Pathfinding (Created Node class and a script to set walkable nodes)

---
+++ ProjetoFinal/Assets/Scripts/Pathfinding/Node.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Node
+{
+    public Vector3Int gridPos;
+    public int gCost; //Distance from starting node
+    public int hCost; //(heuristic) Distance from end node
+
+    public int FCost => gCost + hCost; //Final cost
+
+    public Node(Vector3Int pos)
+    {
+        gridPos = pos;
+    }
+}
---
+++ ProjetoFinal/Assets/Scripts/Pathfinding/NodeGrid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class NodeGrid : MonoBehaviour
+{
+    [SerializeField] private Tilemap ground;
+    public List<Node> walkableNodes;
+
+    private void Start()
+    {
+        walkableNodes = new List<Node>();
+        SetWalkableNodes();
+    }
+
+    /// <summary>
+    /// Sets walkable nodes depending on the ground tilemap
+    /// </summary>
+    private void SetWalkableNodes()
+    {
+        var cellBounds = ground.cellBounds;
+        Vector3Int min = new Vector3Int(cellBounds.xMin, cellBounds.yMin, 0);
+        Vector3Int max = new Vector3Int(cellBounds.xMax, cellBounds.yMax, 0);
+
+        // Loop each cell within the tilemap boundaries to see which one has a tile
+        for (int x = min.x; x < max.x; x++)
+        {
+            for (int y = min.y; y < max.y; y++)
+            {
+                var current = new Vector3Int(x, y, 0);
+                
+                if (ground.HasTile(current)) walkableNodes.Add(new Node(current)); // Creates a walkable node
+            }
+        }
+    }
+}
