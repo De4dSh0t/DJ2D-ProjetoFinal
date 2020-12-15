22c488d3042bc75c8e17a7e1acfde0bc9e2ca42f
De4dSh0t <ruipsilva2001@hotmail.com>
2020-12-15 19:20:40 +0000

    Added ChooseState

---
+++ ProjetoFinal/Assets/Scripts/AI/StateMachine/States/ChooseState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ChooseState : IState
+{
+    private readonly AISystem aiSystem;
+
+    public ChooseState(AISystem system)
+    {
+        aiSystem = system;
+    }
+    
+    public IEnumerator Execute()
+    {
+        ChooseNextState();
+        yield break;
+    }
+
+    private void ChooseNextState()
+    {
+        int nextState = Random.Range(0, 2);
+
+        switch (nextState)
+        {
+            case 0: // Idle
+            {
+                aiSystem.SetState(new IdleState(aiSystem));
+                break;
+            }
+            case 1: // Find
+            {
+                aiSystem.SetState(new FindState(aiSystem, aiSystem.Pathfinding, aiSystem.PositionInt, aiSystem.WaypointInt));
+                break;
+            }
+        }
+    }
+}
