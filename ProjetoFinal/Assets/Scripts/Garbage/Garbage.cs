a047cc44d56d3883825a1600ae47609be95ba764
De4dSh0t <ruipsilva2001@hotmail.com>
2021-01-05 21:26:57 +0000

    Added Cleaning Time

--- ProjetoFinal/Assets/Scripts/Garbage/Garbage.cs
+++ ProjetoFinal/Assets/Scripts/Garbage/Garbage.cs
@@ -2,6 +2,10 @@
 
 public class Garbage : MonoBehaviour
 {
+    [Header("Time Settings")]
+    [SerializeField] private float cleaningTime;
+    
     public Zone Zone { get; set; }
     public Vector3Int Position { get; set; }
+    public float CleaningTime => cleaningTime;
 }
