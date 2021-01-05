51eea4d4d1c67fda073e5fb9845c951766802b56
De4dSh0t <ruipsilva2001@hotmail.com>
2021-01-05 20:02:29 +0000

    Started working on the garbage pickup mechanics

---
+++ ProjetoFinal/Assets/Scripts/Player/GarbagePickUp.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class GarbagePickUp : MonoBehaviour
+{
+    [Header("Collider Settings")]
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask contactLayer;
+
+    [Header("Player Info Settings")]
+    [SerializeField] private PlayerInfo playerInfo;
+    private Garbage garbageToPick;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        CheckCollision();
+        HandlePickUp();
+    }
+
+    private void HandlePickUp()
+    {
+        if (garbageToPick == null) return;
+        
+        // Pick-Up
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(garbageToPick.gameObject);
+            playerInfo.CarryingCount++;
+        }
+    }
+
+    private void CheckCollision()
+    {
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, radius, Vector2.zero, 0, contactLayer);
+        
+        //Checks if it's touching a garbage object
+        if (hit)
+        {
+            garbageToPick = hit.collider.GetComponent<Garbage>();
+            return;
+        }
+
+        garbageToPick = null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
---
+++ ProjetoFinal/Assets/Scripts/Player/PlayerInfo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    [Header("Carrying Settings")]
+    [SerializeField] private int maxCarryingCapacity;
+    private int carryingCount;
+    
+    /// <summary>
+    /// Current Carrying Count
+    /// </summary>
+    public int CarryingCount
+    {
+        get => carryingCount;
+        set
+        {
+            if (carryingCount + value >= maxCarryingCapacity)
+            {
+                carryingCount = maxCarryingCapacity;
+                return;
+            }
+            
+            carryingCount += value;
+        }
+    }
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
