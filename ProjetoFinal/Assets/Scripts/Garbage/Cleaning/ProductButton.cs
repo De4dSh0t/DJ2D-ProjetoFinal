a7fa60644ff3fcfe986d68c47498451f4771e1cd
De4dSh0t <ruipsilva2001@hotmail.com>
2021-01-10 15:57:12 +0000

    Added Hiring Menu and Cleaning Products Store

---
+++ ProjetoFinal/Assets/Scripts/Garbage/Cleaning/ProductButton.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ProductButton : MonoBehaviour
+{
+    [SerializeField] private TMP_Text title;
+    [SerializeField] private TMP_Text numUses;
+    [SerializeField] private TMP_Text price;
+    
+    public void Setup(string id, int nUses, int cost)
+    {
+        title.text = id;
+        numUses.text = nUses.ToString();
+        price.text = cost.ToString();
+    }
+}
---
+++ ProjetoFinal/Assets/Scripts/Garbage/Cleaning/ProductStore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductStore : MonoBehaviour
+{
+    [Header("Products Settings")]
+    [SerializeField] private List<CleaningProduct> cleaningProducts;
+    
+    [Header("Prefab Settings")]
+    [SerializeField] private GameObject productPrefab;
+    
+    [Header("UI Settings")]
+    [SerializeField] private GameObject content;
+
+    private void Start()
+    {
+        DisplayList();
+    }
+
+    private void DisplayList()
+    {
+        foreach (var product in cleaningProducts)
+        {
+            GameObject pButton = Instantiate(productPrefab, content.transform);
+            pButton.GetComponent<ProductButton>().Setup(product.ProductID, product.NumberOfUses, product.Cost);
+        }
+    }
+}
