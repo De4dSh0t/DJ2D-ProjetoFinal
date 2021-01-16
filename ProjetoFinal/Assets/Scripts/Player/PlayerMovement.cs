e3c0588c4e27f68e4ad3a36bfeca88d708c12e13
De4dSh0t <ruipsilva2001@hotmail.com>
2021-01-16 18:04:06 +0000

    Imported characters, Added some animations and Updated PlayerMovement to handle new movement animation

--- ProjetoFinal/Assets/Scripts/Player/PlayerMovement.cs
+++ ProjetoFinal/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,8 +2,14 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Settings")]
     [SerializeField] private float speed;
     private CollisionDetector collisionDetector;
+    
+    [Header("Animation Settings")]
+    [SerializeField] private Animator animator;
+    private Vector2 movementVector;
+    private bool isWalking;
     
     //Movement Commands
     private MoveUp up;
@@ -28,24 +34,41 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandleMoveInput()
     {
+        movementVector = Vector2.zero;
+        isWalking = false;
+        
         if (Input.GetKey(KeyCode.W))
         {
             if (collisionDetector.canMoveUp) up.Execute();
+            movementVector += Vector2.up;
+            isWalking = true;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             if (collisionDetector.canMoveLeft) left.Execute();
+            movementVector += Vector2.left;
+            isWalking = true;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             if (collisionDetector.canMoveDown) down.Execute();
+            movementVector += Vector2.down;
+            isWalking = true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             if (collisionDetector.canMoveRight) right.Execute();
+            movementVector += Vector2.right;
+            isWalking = true;
         }
+        
+        // Animation
+        Vector2 normalized = movementVector.normalized;
+        animator.SetBool("isWalking", isWalking);
+        animator.SetFloat("Horizontal", normalized.x);
+        animator.SetFloat("Vertical", normalized.y);
     }
 }
