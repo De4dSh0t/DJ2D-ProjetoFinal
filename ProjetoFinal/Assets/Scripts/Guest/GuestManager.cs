910df2133264d2befe5364497b23cacd2b88eb16
De4dSh0t <ruipsilva2001@hotmail.com>
2021-01-16 11:07:23 +0000

    Added GuestManager (with spawner) and TimeManager

---
+++ ProjetoFinal/Assets/Scripts/Guest/GuestManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GuestManager : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    [SerializeField] private GameObject guestPrefab;
+    [SerializeField] private Zone spawnZone;
+    [SerializeField] [Tooltip("X: Min | Y: Max")] private Vector2 spawnRate;
+    private readonly List<GameObject> spawnedGuests = new List<GameObject>();
+    
+    private void Start()
+    {
+        StartCoroutine(SpawnGuest());
+    }
+    
+    private IEnumerator SpawnGuest()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(spawnRate.x, spawnRate.y));
+            GameObject newGuest = Instantiate(guestPrefab, spawnZone.EntryWaypoint, Quaternion.identity);
+            spawnedGuests.Add(newGuest);
+        }
+    }
+}
---
+++ ProjetoFinal/Assets/Scripts/Time/TimeManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] private float levelTime;
+    [SerializeField] private int startsAt = 9;
+    [SerializeField] private int endsAt = 21;
+    private float elapsedTime;
+    private int currentHour;
+    private float convertedTime;
+    private float rate;
+    
+    private void Start()
+    {
+        HandleTimeConversion();
+        currentHour = startsAt;
+    }
+    
+    private void Update()
+    {
+        if (elapsedTime >= rate)
+        {
+            currentHour++;
+            rate += convertedTime;
+            print(currentHour);
+        }
+        elapsedTime += Time.deltaTime;
+    }
+    
+    private void HandleTimeConversion()
+    {
+        int hours = Mathf.Abs(endsAt - startsAt);
+        convertedTime = levelTime / hours;
+        rate = convertedTime;
+    }
+}
