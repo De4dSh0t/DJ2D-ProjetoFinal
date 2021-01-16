247b9e3dbcd993462fd100d97948986ce7d690e1
De4dSh0t <ruipsilva2001@hotmail.com>
2021-01-16 15:02:23 +0000

    Updated GuestManager to spawn special guests

--- ProjetoFinal/Assets/Scripts/Guest/GuestManager.cs
+++ ProjetoFinal/Assets/Scripts/Guest/GuestManager.cs
@@ -7,9 +7,15 @@ public class GuestManager : MonoBehaviour
 {
     [Header("Spawn Settings")]
     [SerializeField] private GameObject guestPrefab;
+    [SerializeField] private GameObject specialGuestPrefab;
     [SerializeField] private Zone spawnZone;
     [SerializeField] [Tooltip("X: Min | Y: Max")] private Vector2 spawnRate;
     private readonly List<GameObject> spawnedGuests = new List<GameObject>();
+    
+    [Header("Special Guests Settings")]
+    [SerializeField] private bool canSpawnSpecial;
+    [SerializeField] private int maxNumSpecial;
+    private int nSpecialGuests;
     
     private void Start()
     {
@@ -21,8 +27,22 @@ public class GuestManager : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(spawnRate.x, spawnRate.y));
-            GameObject newGuest = Instantiate(guestPrefab, spawnZone.EntryWaypoint, Quaternion.identity);
-            spawnedGuests.Add(newGuest);
+            
+            int i = 0;
+            if (canSpawnSpecial && nSpecialGuests <= maxNumSpecial) i = Random.Range(0, 2);
+            
+            switch (i)
+            {
+                case 0:
+                    GameObject normalGuest = Instantiate(guestPrefab, spawnZone.EntryWaypoint, Quaternion.identity);
+                    spawnedGuests.Add(normalGuest);
+                    break;
+                case 1:
+                    GameObject specialGuest = Instantiate(specialGuestPrefab, spawnZone.EntryWaypoint, Quaternion.identity);
+                    spawnedGuests.Add(specialGuest);
+                    nSpecialGuests++;
+                    break;
+            }
         }
     }
 }
