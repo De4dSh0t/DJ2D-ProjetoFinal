9edbf7f68de94a1577a8d3ba305032a2fe4eddfa
De4dSh0t <ruipsilva2001@hotmail.com>
2021-01-17 11:03:42 +0000

    Minor animation update

--- ProjetoFinal/Assets/Scripts/AI/StateMachine/States/Cleaning/CleanState.cs
+++ ProjetoFinal/Assets/Scripts/AI/StateMachine/States/Cleaning/CleanState.cs
@@ -17,6 +17,9 @@ public class CleanState : IState
         // Waits for the AI entity to reach its destination
         yield return aiSystem.SetState(new MoveState(aiSystem, aiSystem.Pathfinding, aiSystem.PositionInt, garbageToCollect.Position));
         
+        // Stops walking animation
+        aiSystem.Animator.SetBool("isWalking", false);
+        
         // Cleaning Time
         yield return new WaitForSeconds(garbageToCollect.CleaningTime);
         Clean();
