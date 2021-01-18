64ba668f60ce934a3a788272479395e9e0c5f123
De4dSh0t <ruipsilva2001@hotmail.com>
2021-01-18 01:32:30 +0000

    Added Options and Added a level

---
+++ ProjetoFinal/Assets/Scripts/Audio/AudioSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioSettings", menuName = "ScriptableObjects/AudioSettings", order = 4)]
+public class AudioSettings : ScriptableObject
+{
+    public bool sfx;
+    public bool music;
+    public float sfxVolume;
+    public float musicVolume;
+}
---
+++ ProjetoFinal/Assets/Scripts/UI/Menu/Options/Options.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    [Header("UI Settings")]
+    [SerializeField] private Slider sfx;
+    [SerializeField] private Slider music;
+    [SerializeField] private Toggle sfxToggle;
+    [SerializeField] private Toggle musicToggle;
+    
+    [Header("Mixer Settings")]
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private AudioSettings audioSettings;
+    
+    private void Start()
+    {
+        sfx.onValueChanged.AddListener(UpdateSfxVolume);
+        music.onValueChanged.AddListener(UpdateMusicVolume);
+        sfxToggle.onValueChanged.AddListener(UpdateSfx);
+        musicToggle.onValueChanged.AddListener(UpdateMusic);
+        
+        sfx.value = audioSettings.sfxVolume;
+        music.value = audioSettings.musicVolume;
+        sfxToggle.isOn = audioSettings.sfx;
+        musicToggle.isOn = audioSettings.music;
+    }
+    
+    private void UpdateSfxVolume(float value)
+    {
+        mixer.SetFloat("sfxVolume", Mathf.Log10(value) * 20);
+        audioSettings.sfxVolume = value;
+    }
+    
+    private void UpdateMusicVolume(float value)
+    {
+        mixer.SetFloat("musicVolume", Mathf.Log10(value) * 20);
+        audioSettings.musicVolume = value;
+    }
+    
+    private void UpdateSfx(bool state)
+    {
+        switch (state)
+        {
+            case true:
+                UpdateSfxVolume(audioSettings.sfxVolume);
+                break;
+            case false:
+                mixer.SetFloat("sfxVolume", -80f);
+                break;
+        }
+        
+        audioSettings.sfx = state;
+    }
+    
+    private void UpdateMusic(bool state)
+    {
+        switch (state)
+        {
+            case true:
+                UpdateMusicVolume(audioSettings.musicVolume);
+                break;
+            case false:
+                mixer.SetFloat("musicVolume", -80f);
+                break;
+        }
+        
+        audioSettings.music = state;
+    }
+}
