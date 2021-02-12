0ee622d9235bb9c89b30e36f12637fda0d99b6b7
De4dSh0t <ruipsilva2001@hotmail.com>
2021-02-12 18:41:51 +0000

    Added Logo scene

---
+++ ProjetoFinal/Assets/Scripts/UI/SceneTransition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    [SerializeField] private string nextScene;
+    [SerializeField] private float timeToTransit;
+    private float timer;
+    
+    void Start()
+    {
+        timer = timeToTransit;
+    }
+    
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
