7b02606e16e8a5272c95d988a5607dd6718d6291
De4dSh0t <ruipsilva2001@hotmail.com>
2021-02-12 18:26:46 +0000

    Major Update
    1. Added music for level 2 and level 3
    2. Added EndGame scene
    3. Minor logic updates

---
+++ ProjetoFinal/Assets/Scripts/UI/EndScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    // Timer Settings
+    private float startingTime;
+    
+    void Start()
+    {
+        startingTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time - startingTime <= 2) return;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space) 
+            || Time.time - startingTime >= 16)
+        {
+            SaveManager.DeleteSave();
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
